5a751b38d5ef54beeab1ac0fe96e69bfb6a31f0d
HiImVOID <raphaelluz.void@gmail.com>
2023-07-26 21:34:20 -0300

    Build 11

--- src/components/portifolio/services/dataPortifolio.tsx
+++ src/components/portifolio/services/dataPortifolio.tsx
@@ -1,34 +1,34 @@
-
-
 const dataPortifolio = [
-    {
-        id: 1,
-        name: 'Biblioteca Ocultista  ',
-        title: 'Blog / Ecommerce',
-        description: 'Biblioteca Ocultista é uma vitrine de artigos e produtos do universo mistico e esóterico.',
-        photo: '/portifolio/Screenshot_1.jpg',
-        linkLive: 'https://www.bibliotecaocultista.com/',
-        linkGit: 'https://github.com/HiImVOID/biblioteca-ocultista',
-    },
-    {
-        id: 2,
-        name: 'Soma Projetos Comerciais',
-        title: 'Catalogo de Produtos',
-        description: 'Soma atua com projetos e instalações de mobilias comeriais. ',
-        photo: '/portifolio/Screenshot_2.jpg',
-        linkLive: 'https://www.bibliotecaocultista.com/',
-        linkGit: 'https://github.com/HiImVOID/somaProjetosComerciais',
-    },
-    {
-        id: 3,
-        name: 'Lux ',
-        title: 'Rede Social',
-        description: 'Projeto Pessoal que venho desenvolvendo a fim de evoluir minhas habilidades como desenvolvedor.',
-        photo: '/portifolio/Screenshot_3.jpg',
-        linkLive: 'https://www.bibliotecaocultista.com/',
-        linkGit: 'https://github.com/HiImVOID/biblioteca-ocultista',
-    },
-
+	{
+		id: 1,
+		name: 'Biblioteca Ocultista  ',
+		title: 'Blog / Ecommerce',
+		description:
+			'Biblioteca Ocultista é uma vitrine de artigos e produtos do universo mistico e esóterico.',
+		photo: '/portifolio/Screenshot_1.jpg',
+		linkLive: 'https://www.bibliotecaocultista.com/',
+		linkGit: 'https://github.com/HiImVOID/biblioteca-ocultista',
+	},
+	{
+		id: 2,
+		name: 'Soma Projetos Comerciais',
+		title: 'Catalogo de Produtos',
+		description:
+			'Soma atua com projetos e instalações de mobilias comeriais. ',
+		photo: '/portifolio/Screenshot_2.jpg',
+		linkLive: 'https://www.bibliotecaocultista.com/',
+		linkGit: 'https://github.com/HiImVOID/somaProjetosComerciais',
+	},
+	{
+		id: 3,
+		name: 'Lux ',
+		title: 'Rede Social',
+		description:
+			'Projeto Pessoal que venho desenvolvendo a fim de evoluir minhas habilidades como desenvolvedor.',
+		photo: '/portifolio/Screenshot_3.jpg',
+		linkLive: 'https://www.bibliotecaocultista.com/',
+		linkGit: 'https://github.com/HiImVOID/biblioteca-ocultista',
+	},
 ]
 
 export default dataPortifolio
