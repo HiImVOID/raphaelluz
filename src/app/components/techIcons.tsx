dcb256b6c3a55d602f277f1cd8125b94d1c36e6b
HiImVOID <raphaelluz.void@gmail.com>
2024-04-21 09:51:47 -0300

    Add slide animation

--- src/app/components/techIcons.tsx
+++ src/app/components/techIcons.tsx
@@ -16,149 +16,293 @@ import HTML_icon from '@/../public/tecnologias/HTML_icon.png'
 
 export default function TechIcons() {
 	return (
-		<section className='flex  flex-col items-center w-full gap-y-20 py-3'>
+		<section className='flex  flex-col items-center w-full gap-y-20 py-3 overflow-hidden'>
 			{/* 	<h6 className='font-bold text-2xl mb-4'>Tecnologias:</h6> */}
 
-			<div className='flex flex-wrap justify-around w-full gap-x-3 gap-y-8 px-2 '>
-				<div className='flex flex-col items-center '>
-					<Image
-						src={Git_icon}
-						width={200}
-						height={200}
-						alt='Git'
-						className='w-10 h-10  '
-					/>
-					<span className='font-semibold'>Git</span>
+			<div className='flex w-full  whitespace-nowrap'>
+				<div className='flex justify-around w-full gap-x-3 gap-y-8 px-2 animate-slide'>
+					<div className='flex flex-col items-center  min-w-max'>
+						<Image
+							src={Git_icon}
+							width={200}
+							height={200}
+							alt='Git'
+							className='w-10 h-10  '
+						/>
+						<span className='font-semibold'>Git</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={HTML_icon}
+							width={200}
+							height={200}
+							alt='HTML'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>HTML</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={CSS_icon}
+							width={200}
+							height={200}
+							alt='CSS'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>CSS</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={JS_icon}
+							width={200}
+							height={200}
+							alt='JavaScript'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>JavaScript</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={TS_icon}
+							width={200}
+							height={200}
+							alt='TypeScript'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>TypeScript</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={React_icon}
+							width={200}
+							height={200}
+							alt='React'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>React</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={Next_icon}
+							width={200}
+							height={200}
+							alt='NextJs'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>NextJs</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={Tailwind_icon}
+							width={200}
+							height={200}
+							alt='Git'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>Tailwind CSS</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={BootsTrap_icon}
+							width={200}
+							height={200}
+							alt='Bootstrap'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>Bootstrap</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={API_icon}
+							width={200}
+							height={200}
+							alt='API Rest'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>API Rest</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={Node_icon}
+							width={200}
+							height={200}
+							alt='Node'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>Node</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={Python_icon}
+							width={200}
+							height={200}
+							alt='Python'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>Python</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={SQL_icon}
+							width={200}
+							height={200}
+							alt='SQL'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>SQL</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={Mongo_icon}
+							width={200}
+							height={200}
+							alt='MongoDB'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>MongoDB</span>
+					</div>
 				</div>
-				<div className='flex flex-col items-center'>
-					<Image
-						src={HTML_icon}
-						width={200}
-						height={200}
-						alt='HTML'
-						className='w-10 h-10 '
-					/>
-					<span className='font-semibold'>HTML</span>
-				</div>
-				<div className='flex flex-col items-center'>
-					<Image
-						src={CSS_icon}
-						width={200}
-						height={200}
-						alt='CSS'
-						className='w-10 h-10 '
-					/>
-					<span className='font-semibold'>CSS</span>
-				</div>
-				<div className='flex flex-col items-center'>
-					<Image
-						src={JS_icon}
-						width={200}
-						height={200}
-						alt='JavaScript'
-						className='w-10 h-10 '
-					/>
-					<span className='font-semibold'>JavaScript</span>
-				</div>
-				<div className='flex flex-col items-center'>
-					<Image
-						src={TS_icon}
-						width={200}
-						height={200}
-						alt='TypeScript'
-						className='w-10 h-10 '
-					/>
-					<span className='font-semibold'>TypeScript</span>
-				</div>
-				<div className='flex flex-col items-center'>
-					<Image
-						src={React_icon}
-						width={200}
-						height={200}
-						alt='React'
-						className='w-10 h-10 '
-					/>
-					<span className='font-semibold'>React</span>
-				</div>
-				<div className='flex flex-col items-center'>
-					<Image
-						src={Next_icon}
-						width={200}
-						height={200}
-						alt='NextJs'
-						className='w-10 h-10 '
-					/>
-					<span className='font-semibold'>NextJs</span>
-				</div>
-				<div className='flex flex-col items-center'>
-					<Image
-						src={Tailwind_icon}
-						width={200}
-						height={200}
-						alt='Git'
-						className='w-10 h-10 '
-					/>
-					<span className='font-semibold'>Tailwind CSS</span>
-				</div>
-				<div className='flex flex-col items-center'>
-					<Image
-						src={BootsTrap_icon}
-						width={200}
-						height={200}
-						alt='Bootstrap'
-						className='w-10 h-10 '
-					/>
-					<span className='font-semibold'>Bootstrap</span>
-				</div>
-				<div className='flex flex-col items-center'>
-					<Image
-						src={API_icon}
-						width={200}
-						height={200}
-						alt='API Rest'
-						className='w-10 h-10 '
-					/>
-					<span className='font-semibold'>API Rest</span>
-				</div>
-				<div className='flex flex-col items-center'>
-					<Image
-						src={Node_icon}
-						width={200}
-						height={200}
-						alt='Node'
-						className='w-10 h-10 '
-					/>
-					<span className='font-semibold'>Node</span>
-				</div>
-				<div className='flex flex-col items-center'>
-					<Image
-						src={Python_icon}
-						width={200}
-						height={200}
-						alt='Python'
-						className='w-10 h-10 '
-					/>
-					<span className='font-semibold'>Python</span>
-				</div>
-				<div className='flex flex-col items-center'>
-					<Image
-						src={SQL_icon}
-						width={200}
-						height={200}
-						alt='SQL'
-						className='w-10 h-10 '
-					/>
-					<span className='font-semibold'>SQL</span>
-				</div>
-				<div className='flex flex-col items-center'>
-					<Image
-						src={Mongo_icon}
-						width={200}
-						height={200}
-						alt='MongoDB'
-						className='w-10 h-10 '
-					/>
-					<span className='font-semibold'>MongoDB</span>
+				<div className='flex justify-around w-full gap-x-3 gap-y-8 px-2 animate-slide'>
+					<div className='flex flex-col items-center min-w-max '>
+						<Image
+							src={Git_icon}
+							width={200}
+							height={200}
+							alt='Git'
+							className='w-10 h-10  '
+						/>
+						<span className='font-semibold'>Git</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={HTML_icon}
+							width={200}
+							height={200}
+							alt='HTML'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>HTML</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={CSS_icon}
+							width={200}
+							height={200}
+							alt='CSS'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>CSS</span>
+					</div>
+					<div className='flex flex-col items-center  min-w-max'>
+						<Image
+							src={JS_icon}
+							width={200}
+							height={200}
+							alt='JavaScript'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>JavaScript</span>
+					</div>
+					<div className='flex flex-col items-center  min-w-max'>
+						<Image
+							src={TS_icon}
+							width={200}
+							height={200}
+							alt='TypeScript'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>TypeScript</span>
+					</div>
+					<div className='flex flex-col items-center  min-w-max'>
+						<Image
+							src={React_icon}
+							width={200}
+							height={200}
+							alt='React'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>React</span>
+					</div>
+					<div className='flex flex-col items-center  min-w-max'>
+						<Image
+							src={Next_icon}
+							width={200}
+							height={200}
+							alt='NextJs'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>NextJs</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={Tailwind_icon}
+							width={200}
+							height={200}
+							alt='Git'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>Tailwind CSS</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={BootsTrap_icon}
+							width={200}
+							height={200}
+							alt='Bootstrap'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>Bootstrap</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={API_icon}
+							width={200}
+							height={200}
+							alt='API Rest'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>API Rest</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={Node_icon}
+							width={200}
+							height={200}
+							alt='Node'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>Node</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={Python_icon}
+							width={200}
+							height={200}
+							alt='Python'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>Python</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={SQL_icon}
+							width={200}
+							height={200}
+							alt='SQL'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>SQL</span>
+					</div>
+					<div className='flex flex-col items-center min-w-max'>
+						<Image
+							src={Mongo_icon}
+							width={200}
+							height={200}
+							alt='MongoDB'
+							className='w-10 h-10 '
+						/>
+						<span className='font-semibold'>MongoDB</span>
+					</div>
 				</div>
 			</div>
 		</section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,14 @@ module.exports = {
 	],
 	theme: {
 		extend: {
+			keyframes: {
+				slide: {
+					'0%': { transform: 'translateX(0)' },
+					'100%': { transform: 'translateX(-100%)' },
+				},
+			},
 			animation: {
-				slide: '',
+				slide: '20s slide infinite linear',
 			},
 			backgroundImage: {
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
